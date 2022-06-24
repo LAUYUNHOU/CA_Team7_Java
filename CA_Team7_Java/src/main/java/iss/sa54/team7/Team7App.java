5d5487d64207917e800aa171a8aba11d76e44087
Eugene Ong <eugeneongjj@gmail.com>
2022-06-24 16:12:58 +0800

    student-view all courses done

--- CA_Team7_Java/src/main/java/iss/sa54/team7/Team7App.java
+++ CA_Team7_Java/src/main/java/iss/sa54/team7/Team7App.java
@@ -30,24 +30,25 @@ public class Team7App {
 
 	   };
 	   
-	   @Bean 
-       public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-       return args ->{SCryptPasswordEncoder encoder = new SCryptPasswordEncoder(); String
-       hashedPassword = encoder.encode("mmm"); String hashedPassword1 =
-       encoder.encode("ttt"); String hashedPassword2 = encoder.encode("jjj");
-
-       User user1 = new User("Mike", hashedPassword, "Mike", RoleType.STUDENT); User
-       user2 = new User("Tin", hashedPassword1, "Tin", RoleType.LECTURER); User
-       user3 = new User("Jean", hashedPassword2, "Jean",RoleType.ADMIN);
-       uService.createUser(user1); uService.createUser(user2);
-       uService.createUser(user3);
-
-       LocalDate dt1 = LocalDate
-       .of(2022, 6, 15); LocalDate dt2 = LocalDate .of(2022, 12, 15); LocalDate dt3
-       = LocalDate .of(2022, 4, 30); LocalDate dt4 = LocalDate .of(2023, 7, 20);
-
-       Course c1 = new Course("Java Spring",dt1,dt2, 3, 50, 2); Course c2 = new
-       Course("Python ML", dt3,dt4, 4, 40, 2); cService.createCourse(c1);
-       cService.createCourse(c2);}; }}
+		/*
+		 * @Bean public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		 * return args ->{SCryptPasswordEncoder encoder = new SCryptPasswordEncoder();
+		 * String hashedPassword = encoder.encode("mmm"); String hashedPassword1 =
+		 * encoder.encode("ttt"); String hashedPassword2 = encoder.encode("jjj");
+		 * 
+		 * User user1 = new User("Mike", hashedPassword, "Mike", RoleType.STUDENT); User
+		 * user2 = new User("Tin", hashedPassword1, "Tin", RoleType.LECTURER); User
+		 * user3 = new User("Jean", hashedPassword2, "Jean",RoleType.ADMIN);
+		 * uService.createUser(user1); uService.createUser(user2);
+		 * uService.createUser(user3);
+		 * 
+		 * LocalDate dt1 = LocalDate .of(2022, 6, 15); LocalDate dt2 = LocalDate
+		 * .of(2022, 12, 15); LocalDate dt3 = LocalDate .of(2022, 4, 30); LocalDate dt4
+		 * = LocalDate .of(2023, 7, 20);
+		 * 
+		 * Course c1 = new Course("Java Spring",dt1,dt2, 3, 50, 2); Course c2 = new
+		 * Course("Python ML", dt3,dt4, 4, 40, 2); cService.createCourse(c1);
+		 * cService.createCourse(c2);}; }
+		 */}
 
 	
